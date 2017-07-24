a2f823c2fe4df9f11912160b4200bf3c56f9760b
Jimmy Jia <tesrin@gmail.com>
2017-07-24 17:04:34 -0400

    Fix running in compat mode

---
+++ test/compat/smoke.test.js
@@ -0,0 +1,63 @@
+import createRender from 'found/lib/createRender';
+import getFarceResult from 'found/lib/server/getFarceResult';
+import ReactTestUtils from 'react-dom/test-utils';
+import Relay from 'react-relay/classic';
+import { graphql } from 'react-relay/compat';
+
+import { Resolver as ModernResolver } from '../../src';
+import { Resolver as ClassicResolver } from '../../src/classic';
+
+import { createEnvironment as createClassicEnvironment }
+  from '../classic/helpers';
+import { createEnvironment as createModernEnvironment }
+  from '../modern/helpers';
+
+describe('smoke', () => {
+  it('should work with modern resolver', async () => {
+    const { element } = await getFarceResult({
+      url: '/',
+      routeConfig: [{
+        path: '/',
+        Component: () => null,
+        query: graphql`
+          query smoke_Query {
+            widget {
+              name
+            }
+          }
+        `,
+      }],
+      resolver: new ModernResolver(createModernEnvironment()),
+      render: createRender({}),
+    });
+
+    ReactTestUtils.renderIntoDocument(element);
+  });
+
+  it('should work with classic resolver', async () => {
+    const Container = Relay.createContainer(() => null, {
+      fragments: {
+        widget: () => Relay.QL`
+          fragment on Widget {
+            name
+          }
+        `,
+      },
+    });
+
+    const { element } = await getFarceResult({
+      url: '/',
+      routeConfig: [{
+        path: '/',
+        Component: Container,
+        queries: {
+          widget: () => Relay.QL`query { widget }`,
+        },
+      }],
+      resolver: new ClassicResolver(createClassicEnvironment()),
+      render: createRender({}),
+    });
+
+    ReactTestUtils.renderIntoDocument(element);
+  });
+});
