96e1d3cdd46d8b10f269a47c9c0359a358b2af8a
Jimmy Jia <tesrin@gmail.com>
2017-10-03 15:49:08 -0400

    Fix disposing pending requests

--- test/modern/navigation.test.js
+++ test/modern/navigation.test.js
@@ -8,11 +8,73 @@ import { graphql } from 'react-relay';
 
 import { createEnvironment, InstrumentedResolver } from './helpers';
 
-describe('retain', () => {
+describe('navigation', () => {
   let environment;
 
   beforeEach(() => {
     environment = createEnvironment();
+  });
+
+  it('should support aborting navigation', async () => {
+    const Router = createFarceRouter({
+      historyProtocol: new ServerProtocol('/foo'),
+      routeConfig: [{
+        path: '/:name',
+        query: graphql`
+          query navigation_name_Query($name: String!) {
+            widget: widgetByArg(name: $name) {
+              name
+            }
+          }
+        `,
+        render: ({ props }) => (
+          props && <div className={props.widget.name} />
+        ),
+      }],
+
+      render: createRender({}),
+    });
+
+    const resolver = new InstrumentedResolver(environment);
+    const instance = ReactTestUtils.renderIntoDocument(
+      <Router resolver={resolver} />,
+    );
+
+    await resolver.done;
+
+    ReactTestUtils.findRenderedDOMComponentWithClass(instance, 'foo');
+    expect(
+      ReactTestUtils.scryRenderedDOMComponentsWithClass(instance, 'bar'),
+    ).toHaveLength(0);
+    expect(
+      ReactTestUtils.scryRenderedDOMComponentsWithClass(instance, 'baz'),
+    ).toHaveLength(0);
+
+    // TODO: Use MemoryProtocol once we implement it instead of doing this.
+    instance.store.dispatch({
+      type: FarceActionTypes.UPDATE_LOCATION,
+      payload: {
+        pathname: '/bar',
+      },
+    });
+
+    // Immediately trigger another location update to abort the previous one.
+    instance.store.dispatch({
+      type: FarceActionTypes.UPDATE_LOCATION,
+      payload: {
+        pathname: '/baz',
+      },
+    });
+
+    await resolver.done;
+
+    expect(
+      ReactTestUtils.scryRenderedDOMComponentsWithClass(instance, 'foo'),
+    ).toHaveLength(0);
+    expect(
+      ReactTestUtils.scryRenderedDOMComponentsWithClass(instance, 'bar'),
+    ).toHaveLength(0);
+    ReactTestUtils.findRenderedDOMComponentWithClass(instance, 'baz');
   });
 
   it('should support retaining previous children', async () => {
@@ -53,7 +115,7 @@ describe('retain', () => {
             path: 'foo',
             Component: Widget,
             query: graphql`
-              query retain_foo_Query {
+              query navigation_foo_Query {
                 widget: widgetByArg(name: "foo") {
                   name
                 }
@@ -64,7 +126,7 @@ describe('retain', () => {
             path: 'bar',
             Component: Widget,
             query: graphql`
-              query retain_bar_Query {
+              query navigation_bar_Query {
                 widget: widgetByArg(name: "bar") {
                   name
                 }
