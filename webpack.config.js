270c2edd466d0183ea21ce494dfc92d7b4f73459
Jimmy Jia <tesrin@gmail.com>
2019-12-20 13:13:40 -0500

    Squashed 'examples/todomvc/' content from commit 0f0c7ae
    git-subtree-dir: examples/todomvc
    git-subtree-split: 0f0c7aeb6f59d5cef2722f444092f9034cdaf2f5

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = (env, { mode }) => ({
+  entry: './src/client',
+
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Relay • TodoMVC',
+    }),
+    new CopyWebpackPlugin(['src/assets']),
+  ],
+
+  devtool: mode === 'production' ? 'source-map' : 'cheap-module-source-map',
+});
