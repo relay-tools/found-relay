d986da714484acdc93c643d1177af4af899767a4
Jimmy Jia <tesrin@gmail.com>
2019-12-20 13:13:50 -0500

    Squashed 'examples/todomvc-universal/' content from commit ef3e332
    git-subtree-dir: examples/todomvc-universal
    git-subtree-split: ef3e332678858b36c4f4b66167bde7696de3f26c

---
+++ src/mutations/MarkAllTodosMutation.js
@@ -0,0 +1,78 @@
+import { commitMutation, graphql } from 'react-relay';
+import { ConnectionHandler } from 'relay-runtime';
+
+const mutation = graphql`
+  mutation MarkAllTodosMutation($input: MarkAllTodosInput!, $status: String!) {
+    markAllTodos(input: $input) {
+      viewer {
+        todos(status: $status) {
+          edges {
+            node {
+              id
+              complete
+              text
+            }
+          }
+        }
+        id
+        numCompletedTodos
+      }
+      changedTodos {
+        id
+        complete
+      }
+    }
+  }
+`;
+
+function commit(environment, user, complete, status) {
+  return commitMutation(environment, {
+    mutation,
+    variables: {
+      input: { complete },
+      status,
+    },
+
+    updater(store) {
+      const userProxy = store.get(user.id);
+      const connection = ConnectionHandler.getConnection(
+        userProxy,
+        'TodoList_todos',
+        { status },
+      );
+      const todoEdges = store
+        .getRootField('markAllTodos')
+        .getLinkedRecord('viewer')
+        .getLinkedRecord('todos', { status })
+        .getLinkedRecords('edges');
+      connection.setLinkedRecords(todoEdges, 'edges');
+    },
+
+    optimisticUpdater(store) {
+      const userProxy = store.get(user.id);
+      const connection = ConnectionHandler.getConnection(
+        userProxy,
+        'TodoList_todos',
+        { status },
+      );
+
+      if (
+        (complete && status === 'active') ||
+        (!complete && status === 'completed')
+      ) {
+        connection.setLinkedRecords([], 'edges');
+      }
+
+      connection.getLinkedRecords('edges').forEach(edge => {
+        edge.getLinkedRecord('node').setValue(complete, 'complete');
+      });
+
+      userProxy.setValue(
+        complete ? userProxy.getValue('numTodos') : 0,
+        'numCompletedTodos',
+      );
+    },
+  });
+}
+
+export default { commit };
